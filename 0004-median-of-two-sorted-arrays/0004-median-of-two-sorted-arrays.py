050f9e6a813d72af2e5b759fcdb4b866117277a6
Shashikant Yadav <112337020+shashican17@users.noreply.github.com>
2026-08-11 14:03:24 +0530

    Time: 2 ms (51.28%), Space: 19.6 MB (41.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -2,27 +2,26 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n1 = len(nums1)
         n2 = len(nums2)
-        median1 = 0
-        median2 = 0
-        i1 = 0
-        i2 = 0
-        for count in range( (n1 + n2) // 2 + 1):
-            median2 = median1
-            if i1 < n1 and i2 < n2:
-                if nums1[i1] > nums2[i2]:
-                    median1 = nums2[i2]
-                    i2 += 1
-                else:
-                    median1 = nums1[i1]
-                    i1 += 1
-            elif i1 < n1:
-                median1 = nums1[i1]
-                i1 += 1
-            else:
-                median1 = nums2[i2]
-                i2 += 1
+        i , j = 0, 0
+        median1 , median2 = 0, 0
 
+        for count in range( (n1 + n2) // 2 + 1 ):
+            median2 = median1
+            if i < n1 and j < n2:
+                if nums1[i] > nums2[j]:
+                    median1 = nums2[j]
+                    j += 1
+                else:
+                    median1 = nums1[i]
+                    i += 1
+            elif i < n1:
+                median1 = nums1[i]
+                i += 1
+            else:
+                median1 = nums2[j]
+                j += 1
+        
         if (n1 + n2) % 2 == 1:
-            return float(median1)
+            return median1
         else:
             return (median1 + median2) / 2
