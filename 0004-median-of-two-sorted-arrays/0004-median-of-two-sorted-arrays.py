1bb4f581b14ac4848aad9a87bf992ab4c7f494d2
Shashikant Yadav <112337020+shashican17@users.noreply.github.com>
2026-08-11 14:46:23 +0530

    Time: 7 ms (12.54%), Space: 19.6 MB (14.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,27 +1,24 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n1 = len(nums1)
-        n2 = len(nums2)
-        i , j = 0, 0
-        median1 , median2 = 0, 0
+    def get_kth(self, a : List[int], m : int, b : List[int], n : int, k : int, a_start : int = 0, b_start : int = 0) -> int :
+        if m > n:
+            return self.get_kth(b, n, a, m, k, b_start, a_start)
 
-        for count in range( (n1 + n2) // 2 + 1 ):
-            median2 = median1
-            if i < n1 and j < n2:
-                if nums1[i] > nums2[j]:
-                    median1 = nums2[j]
-                    j += 1
-                else:
-                    median1 = nums1[i]
-                    i += 1
-            elif i < n1:
-                median1 = nums1[i]
-                i += 1
-            else:
-                median1 = nums2[j]
-                j += 1
+        if m == 0:
+            return b[b_start + k - 1]
         
-        if (n1 + n2) % 2 == 1:
-            return median1
+        if k == 1:
+            return min(a[a_start], b[b_start])
+        
+        i = min(m, k//2)
+        j = min(n, k//2)
+
+        if a[a_start + i - 1] > b[b_start + j - 1]:
+            return self.get_kth(a, m, b, n-j, k-j, a_start, b_start + j)
         else:
-            return (median1 + median2) / 2
+            return self.get_kth(a, m-i, b, n, k-i, a_start + i, b_start)
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        left = (len(nums1) + len(nums2) + 1) // 2
+        right = (len(nums1) + len(nums2) + 2) // 2
+
+        return (self.get_kth(nums1, len(nums1), nums2, len(nums2), left) + self.get_kth(nums1, len(nums1), nums2, len(nums2), right)) / 2.0
