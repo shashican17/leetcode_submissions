16a27d32e5af94c637a3373d2e515beeace27d19
Shashikant Yadav <112337020+shashican17@users.noreply.github.com>
2026-08-19 18:54:13 +0530

    Time: 152 ms (5.23%), Space: 113.8 MB (5.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st;
+        for(int i : nums){
+            if(st.find(i) != st.end()){
+                return true;
+            }
+            st.insert(i);
+        }
+        return false;
+    }
+};
