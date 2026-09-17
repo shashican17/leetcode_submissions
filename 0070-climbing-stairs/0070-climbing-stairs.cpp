687cf828bcc3df608f35f812a3f7c3fd40e1be6d
Shashikant Yadav <112337020+shashican17@users.noreply.github.com>
2026-09-17 21:16:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.89%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1 || n == 2){
+            return n;
+        }
+
+        int n1 = 1;
+        int n2 = 2;
+        int res = 0;
+        for(int i=3;i<=n;i++){
+            res = n1 + n2;
+            n1 = n2;
+            n2 = res;
+        }
+        return res;
+    }
+};
